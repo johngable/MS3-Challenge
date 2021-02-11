582e87426b8dc041dda27ded4d861b6fe2d7596c
John G <jg4863@cs.ship.edu>
2021-02-11 01:52:50 -0500

    Successfully reads a row

--- MS3-Coding/src/main/CSVReader.java
+++ MS3-Coding/src/main/CSVReader.java
@@ -4,28 +4,37 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class CSVReader {
 
-	private String pathToCSV;
-	
-	private CSVReader(String pathToCSV) {
-		this.setPathToCSV(pathToCSV);
-	}
-	
-	public void readCSV() throws FileNotFoundException {
+	public static void readCSV(String pathToCSV){
 		File csvFile = new File(pathToCSV);
 		
 		if(csvFile.isFile()) {
-			BufferedReader csvReader = new BufferedReader(new FileReader(pathToCSV));
+			try {
+				BufferedReader csvReader = new BufferedReader(new FileReader(csvFile));
+				
+				try {
+					String row;
+					while((row = csvReader.readLine()) != null) {
+						System.out.println(row);
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				
+				
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 
-	public String getPathToCSV() {
-		return pathToCSV;
-	}
-
-	public void setPathToCSV(String pathToCSV) {
-		this.pathToCSV = pathToCSV;
+	public static void main(String[] args) {
+		readCSV("./../ms3Interview.csv");
 	}
 }
