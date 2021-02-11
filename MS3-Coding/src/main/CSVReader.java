16640fcb8a07e184edc6377c67d894d8533a3191
John G <jg4863@cs.ship.edu>
2021-02-11 03:33:14 -0500

    Successfully parsing csv and entering valid rows to DB.

--- MS3-Coding/src/main/CSVReader.java
+++ MS3-Coding/src/main/CSVReader.java
@@ -5,6 +5,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CSVReader {
 
@@ -17,16 +20,35 @@ public class CSVReader {
 				
 				try {
 					String row;
+					
+					List<List<String>> goodEntry = new ArrayList<List<String>>();
+					List<List<String>> badEntry = new ArrayList<List<String>>();
+
+					SQLiteDB db = new SQLiteDB();
+					
 					while((row = csvReader.readLine()) != null) {
-						System.out.println(row);
+						String[] dataEntry = row.split(",");
+						if(dataEntry.length==11) {
+							goodEntry.add(Arrays.asList(dataEntry));
+							//db.addBatch(dataEntry[0], dataEntry[1], dataEntry[2], dataEntry[3], dataEntry[4], dataEntry[5], dataEntry[6], Boolean.valueOf(dataEntry[7]), Boolean.valueOf(dataEntry[8]), dataEntry[9]);
+							//System.out.println("Entry "+goodEntry.size()+" added successfully.");
+						}else {
+							badEntry.add(Arrays.asList(dataEntry));
+							//System.out.println("Bad Entry");
+						}
 					}
+
+					System.out.println(goodEntry.size()+" Good Entries");
+					System.out.println(badEntry.size()+" Bad Entries");
+					System.out.println((goodEntry.size()+badEntry.size())+" Total Entries");
+
+					db.insertBatch(goodEntry);
+					
+					
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
-				
-				
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
--- MS3-Coding/src/main/SQLiteDB.java
+++ MS3-Coding/src/main/SQLiteDB.java
@@ -8,6 +8,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SQLiteDB {
 
@@ -18,13 +20,13 @@ public class SQLiteDB {
 		try {
 			Connection conn = DriverManager.getConnection(DBPath);
 			
-			String sql = "Create table ms3Interview("
+			String sql = "Create table IF NOT EXISTS ms3Interview("
 					+ "entry_id INTEGER PRIMARY KEY AUTOINCREMENT,"
 					+ "A varchar(50) NOT NULL,"
 					+ "B varchar(50) NOT NULL,"
 					+ "C varchar(50) NOT NULL,"
 					+ "D varchar(10) NOT NULL,"
-					+ "E varchar(50) NOT NULL,"
+					+ "E varchar(512) NOT NULL,"
 					+ "F varchar(50) NOT NULL,"
 					+ "G varchar(50) NOT NULL,"
 					+ "H boolean NOT NULL,"
@@ -59,38 +61,74 @@ public class SQLiteDB {
 	}
 	
 	
-	public static void insertToDB(String a, String b, String c, String d, String e, String f, String g, Boolean h, Boolean i, String j) {
-		
+//	public String makeStatement(List<String> entry) {
+//		String sql = "INSERT INTO ms3Interview (A, B, C, D, E, F, G, H, I, J)"
+//				+ " VALUES(?,?,?,?,?,?,?,?,?,?);";
+//		
+//		try (Connection conn = DriverManager.getConnection(DBPath);
+//				PreparedStatement statement = conn.prepareStatement(sql);){
+//							statement.setString(0, entry.get(0));
+//							statement.setString(1, entry.get(1));
+//							statement.setString(2, entry.get(2));
+//							statement.setString(3, entry.get(3));
+//							statement.setString(4, entry.get(4));
+//							statement.setString(5, entry.get(5));
+//							statement.setString(6, entry.get(6));
+//							statement.setBoolean(7, Boolean.getBoolean(entry.get(8)));
+//							statement.setBoolean(8, Boolean.getBoolean(entry.get(9)));
+//							statement.setString(9, entry.get(10));
+//							
+//							statement.addBatch();
+//							
+//		}catch(SQLException e) {
+//			e.printStackTrace();
+//		}
+//							
+//							
+//		
+//	}
+	
+	public void insertBatch(List<List<String>> goodEntry) {
 		String sql = "INSERT INTO ms3Interview (A, B, C, D, E, F, G, H, I, J)"
-					+ " VALUES(?,?,?,?,?,?,?,?,?,?);";
+				+ " VALUES(?,?,?,?,?,?,?,?,?,?);";
 		
-		try (Connection conn = DriverManager.getConnection(DBPath);
-				PreparedStatement statement = conn.prepareStatement(sql);){
+		Connection conn;
+		try {
+			conn = DriverManager.getConnection(DBPath);
+			PreparedStatement statement = conn.prepareStatement(sql);
+			conn.setAutoCommit(false);
+			System.out.println("Preparing "+goodEntry.size()+" entries.");
+			for(List<String> entry: goodEntry) {
+				statement.setString(1, entry.get(0));
+				statement.setString(2, entry.get(1));
+				statement.setString(3, entry.get(2));
+				statement.setString(4, entry.get(3));
+				statement.setString(5, entry.get(4));
+				statement.setString(6, entry.get(5));
+				statement.setString(7, entry.get(7));
+				statement.setBoolean(8, Boolean.getBoolean(entry.get(8)));
+				statement.setBoolean(9, Boolean.getBoolean(entry.get(9)));
+				statement.setString(10, entry.get(10));
+				
+				statement.addBatch();
+			}
 			
-					statement.setString(1, a);
-					statement.setString(2, b);
-					statement.setString(3, c);
-					statement.setString(4, d);
-					statement.setString(5, e);
-					statement.setString(6, f);
-					statement.setString(7, g);
-					statement.setBoolean(8, h);
-					statement.setBoolean(9, i);
-					statement.setString(10, j);
+			statement.executeBatch();
+			
+			conn.commit();
+			//System.out.println("Added: " + statement.executeBatch() +" new entries.");
+			statement.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
-					System.out.println("Added " + statement.executeUpdate() +" row.");
-			
-        } catch (SQLException exception) {
-            System.out.println(exception.getMessage());
-        }
 	}
 	
 	
 	public static void main(String[] args) {
 		dropTables();
-		createDB();
-		insertToDB("a", "b", "c", "d", "e", "f", "g", true, false, "j");
-		
+		createDB();		
 	}
 
 
